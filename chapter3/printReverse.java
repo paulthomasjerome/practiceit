7df59e34235116db1902eb705d17481938f291f6
Paul T Jerome <mr.paulthomasjerome@gmail.com>
2019-01-18 23:07:43 -0800

    checking to see if this works

---
+++ chapter3/printReverse.java
@@ -0,0 +1,12 @@
+public static void printReverse(String word)
+{
+	    String temp = "";
+
+	    for(int i = word.length() - 1; i >= 0; i--)
+	    {
+	        temp += word.charAt(i);
+
+	    }
+	    System.out.print(temp);
+
+	}
