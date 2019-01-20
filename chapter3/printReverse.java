3d887051d773f41d69cfb5087083e12884577aee
Paul T Jerome <mr.paulthomasjerome@gmail.com>
2019-01-19 19:10:37 -0800

    Adding the exercise description

--- chapter3/printReverse.java
+++ chapter3/printReverse.java
@@ -1,3 +1,9 @@
+/*
+Write a method called printReverse that accepts a String as its parameter and prints
+the characters in opposite order. For example, a call of printReverse("hello there!");
+should print the following output: !ereht olleh
+*/
+
 public static void printReverse(String word)
 {
 	    String temp = "";
